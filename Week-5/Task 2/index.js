03f2394221e60532cd50e0036a3f5aa1d3c0b346
Sanjai Sakthivel <sanjaisakthivel2000@gamil.com>
2023-02-19 14:57:46 +0530

    feat(week-5): added custom map to the logic

--- Week-5/Task 2/index.js
+++ Week-5/Task 2/index.js
@@ -1,5 +1,50 @@
+class customMap {
+    constructor() {
+        this.object = [];
+    }
+    
+    [Symbol.iterator]() {
+        let count = 0;
+        return {
+            next: () => {
+                const value = count === (this.object.length) ? { value: this.object[count], done: true } : { value: this.object[count], done: false };
+                count += 1;
+                return value;
+            }
+        }
+    }
+
+    set(key, value) {
+        let isKeyExist = false;
+        this.object.forEach((keyValuePair) => {
+            if (keyValuePair.key === key) {
+                keyValuePair.value = value;
+                isKeyExist = true;
+            }
+        })
+        if (!isKeyExist) this.object.push({ key: key, value: value })
+    }
+
+    get(key) {
+        const value = this.object.filter((keyValuePair) => {
+            return key === keyValuePair.key
+        });
+        return value[0].value;
+    }
+
+    has(key) {
+        let isKeyExist = false;
+        this.object.forEach((keyValuePair) => {
+            if (keyValuePair.key === key) {
+                isKeyExist = true;
+            }
+        })
+        return isKeyExist;
+    }
+}
+
 function vowelCount(text) {
-    vowelMap = new Map();
+    vowelMap = new customMap();
     for (let individualCharacter of text.toLocaleLowerCase()) {
         if ("aeio".includes(individualCharacter)) {
             vowelMap.has(individualCharacter) ? vowelMap.set(individualCharacter, vowelMap.get(individualCharacter) + 1) : vowelMap.set(individualCharacter, 1);
